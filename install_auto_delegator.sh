1c22f7f5c7b8c99fb17c16d9b38308485a25ec3e
Pretid <pretid@gmail.com>
2024-04-12 11:06:56 +0200

    Update install_auto_delegator.sh
    switch to english

--- install_auto_delegator.sh
+++ install_auto_delegator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt-get install cron
-# Demande à l'utilisateur de saisir la variable PASSPHRASE
-read -sp "Veuillez entrer votre PASSPHRASE : " PASSPHRASE
+# Ask user for phassphrase
+read -sp "Enter your PASSPHRASE : " PASSPHRASE
 echo
 
-# Stocke la variable PASSPHRASE dans le fichier .bashrc
+# save Passphrase in .bashrc
 echo "export PASSPHRASE=\"$PASSPHRASE\"" >> "$HOME/.bashrc"
 source "$HOME/.bashrc"
 
@@ -20,31 +20,27 @@ galacticad tx distribution withdraw-all-rewards --from $WALLET --chain-id galact
 
 sleep 5
 
-echo 'On met le solde en variable'
+echo 'Save amount in var'
 export AMOUNT=$(galacticad query bank balances $WALLET_ADDRESS | awk '/amount/{print substr($3, 2, length($3)-2)}')
 
-echo 'On redelegue'
+echo 'Delegate'
 {
 echo "$PASSPHRASE"
 echo "$PASSPHRASE"
 } |
 galacticad tx staking delegate $VALOPER_ADDRESS "$AMOUNT"agnet --from $WALLET --chain-id galactica_9302-1 --gas 200000 --gas-prices 10agnet -y"
 
-# Chemin et nom du fichier à créer
 file_path="$HOME/auto-withdraw-redelegue.sh"
 
-# Création du fichier avec le contenu
 echo "$script_content" > "$file_path"
 
-# Rendre le fichier exécutable
 chmod +x "$file_path"
 
-echo "Le fichier auto-withdraw-redelegue.sh a été créé avec succès."
+echo "Script file created with success"
 
-# Configuration de la tâche cron
 cron_job="0 * * * * $HOME/auto-withdraw-redelegue.sh"
 
-# Ajouter la tâche cron
+# Add cron task
 (crontab -l ; echo "$cron_job") | crontab -
 
-echo "La tâche cron a été configurée pour exécuter le script toutes les heures."
+echo "Cron task create and will be executed every hour"
